4f57ee54f7a719e232efd1114dff131e1043a244
Ani Toledo <anamtoledop@gmail.com>
2019-09-30 15:01:50 -0500

    add dashboard button

--- app/src/main/java/com/example/marvelacademysharedpreferences/TutorialActivity.kt
+++ app/src/main/java/com/example/marvelacademysharedpreferences/TutorialActivity.kt
@@ -1,12 +1,20 @@
 package com.example.marvelacademysharedpreferences
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tutorial.*
 
 class TutorialActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorial)
+
+        goToDashboard.setOnClickListener {
+            val dashboardActivity = Intent(this, MainActivity::class.java)
+            startActivity(dashboardActivity)
+            finish()
+        }
     }
 }
